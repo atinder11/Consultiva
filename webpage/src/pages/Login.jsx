443afef777e61129a8c01e63599cd1fcb929cef7
Atinder Kumar <atinderkumar1111@gmail.com>
2024-09-21 04:57:10 +0530

    add txt file

--- webpage/src/pages/Login.jsx
+++ webpage/src/pages/Login.jsx
@@ -3,27 +3,26 @@ import "../assets/css/pagecss/Login.css";
 
 const Login = () => {
   const loginwithgoogle = () => {
-    window.open("https://consultivaapi.vercel.app/auth/google/callback", "_self");
+    window.open("https://consultivaapi.vercel.app/auth/google", "_self");
   };
+
   return (
-    <>
-      <div className="login-page">
-        <h1 style={{ textAlign: "center" }}>Login</h1>
-        <div className="form">
-          <form className="login-form">
-            <input type="text" name="" id="" placeholder="username" />
-            <input type="password" name="" id="" placeholder="password" />
-            <button>Login</button>
-            <p className="message">
-              Not Registerd? <a href="#">Create an account</a>
-            </p>
-          </form>
-          <button className="login-with-google-btn" onClick={loginwithgoogle}>
-            Sign In With Google
-          </button>
-        </div>
+    <div className="login-page">
+      <h1 style={{ textAlign: "center" }}>Login</h1>
+      <div className="form">
+        <form className="login-form">
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button type="submit">Login</button>
+          <p className="message">
+            Not Registered? <a href="#">Create an account</a>
+          </p>
+        </form>
+        <button className="login-with-google-btn" onClick={loginwithgoogle}>
+          Sign In With Google
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
