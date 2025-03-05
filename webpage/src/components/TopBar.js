07d67ecafc832685f75d3f2cc44f322322d52056
Atinder Kumar <atinderkumar1111@gmail.com>
2025-03-06 01:22:38 +0530

    minor fixes

--- webpage/src/components/TopBar.js
+++ webpage/src/components/TopBar.js
@@ -6,7 +6,7 @@ function TopBar() {
       <div className="container d-flex justify-content-between">
         <div className="contact-info d-flex align-items-center">
           <i className="bi bi-envelope"></i>
-          <a href="mailto:contact@example.com">contact@Consultiva.com</a>
+          <a href="mailto:contact@Consultiva.com">contact@Consultiva.com</a>
           <i className="bi bi-phone"></i>
           +91 9821023521
         </div>
